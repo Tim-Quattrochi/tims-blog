74c6b3554145d3f2d678835ef48ae6106fd2ffb3
Tim Quattrochi <timq82@gmail.com>
2022-11-23 01:16:28 -0500

    started blog card, what will blogs look like on All blogs page

---
+++ packages/client/src/components/BlogCard.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+
+export default function ProductCard({ blog }) {
+  console.log(blog);
+  return (
+    <Container className="mb-3" style={{ height: '300px' }}>
+      <Card
+        bg="light"
+        text="dark"
+        style={{ position: 'relative', height: '100%' }}
+      >
+        <Card.Img
+          height={150}
+          src={product.productImage}
+          style={{ objectFit: 'cover' }}
+        />
+        <Card.Body>
+          <Card.Title as="h6" style={{ color: 'info' }}>
+            <Row>
+              <Col>
+                <Link to={`/blogs/${blog._id}`}>{blog.title}</Link>
+              </Col>
+              <Col className="text-right font-weight-bold"></Col>
+            </Row>
+          </Card.Title>
+          <Card.Text style={{ fontSize: '.8rem' }}>
+            {blog.title}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+}

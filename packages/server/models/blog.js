cd241c1cf68b4253641cc2f5ea332359cfb9b0ee
Tim Quattrochi <timq82@gmail.com>
2022-11-23 01:21:52 -0500

    blog model

---
+++ packages/server/models/blog.js
@@ -0,0 +1,46 @@
+import { Schema, model } from 'mongoose';
+
+const blogSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      minLength: 5,
+      maxLength: 50,
+    },
+    description: {
+      type: String,
+      required: true,
+      minLength: 15,
+      maxLength: 500,
+    },
+    posts: [
+      {
+        title: {
+          type: String,
+          required: true,
+          minLength: 5,
+          maxLength: 50,
+        },
+        content: {
+          type: String,
+          required: true,
+          minLength: 20,
+        },
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
+        updatedAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Blog = model('Blog', blogSchema);
+
+export default Blog;

ed0a353c542b793345ff59649ecb097a5f08db82
Tim Quattrochi <timq82@gmail.com>
2022-11-24 14:02:26 -0500

    set greeting to grab name from local storage.

--- packages/client/src/components/Header.js
+++ packages/client/src/components/Header.js
@@ -1,10 +1,32 @@
-import { Nav, Navbar } from 'react-bootstrap';
+import { Nav, Navbar, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 const Header = () => {
+  const [returningUser, setReturningUser] = useState();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const logOut = () => {
+    localStorage.removeItem('blogUser');
+    setIsLoggedIn(false);
+  };
+
+  useEffect(() => {
+    //check local storage to see if returning user
+    const localUser = JSON.parse(localStorage.getItem('blogUser'));
+
+    if (localUser) {
+      setIsLoggedIn(true);
+      setReturningUser(localUser.name);
+    }
+  }, []);
+
+  console.log(isLoggedIn);
   return (
     <Navbar bg="dark" variant="dark">
-      <Navbar.Brand>Tim's Blog</Navbar.Brand>
+      <Navbar.Brand>
+        {returningUser ? `${returningUser}'s Blog` : 'Guest'}
+      </Navbar.Brand>
       <Nav>
         <Nav.Link as={Link} to="/blogs">
           All Blogs
@@ -12,6 +34,15 @@ const Header = () => {
         <Nav.Link as={Link} to="/blogs/create">
           Create Blog
         </Nav.Link>
+        {isLoggedIn ? (
+          <Nav.Link onClick={logOut} as={Link} to="/signin">
+            Log Out
+          </Nav.Link>
+        ) : (
+          <Nav.Link as={Link} to="/signin">
+            Log In
+          </Nav.Link>
+        )}
       </Nav>
     </Navbar>
   );

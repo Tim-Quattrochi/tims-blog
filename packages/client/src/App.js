d83ecd5a565c2fbe345d59b5c41433bbcf845499
Tim Quattrochi <timq82@gmail.com>
2022-11-24 01:29:44 -0500

    app.js added routes

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,7 +1,22 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import CreateBlogPage from './pages/CreateBlogPage';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import BlogPage from './pages/BlogPage';
+import AllBlogs from './pages/AllBlogs';
 
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <Container>
+      <Header />
+      <Routes>
+        <Route path="/blogs" element={<AllBlogs />} />
+        <Route path="/blogs/create" element={<CreateBlogPage />} />
+        <Route path="/blogs/:blogId" element={<BlogPage />} />
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;

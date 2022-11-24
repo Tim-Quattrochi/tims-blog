7eaac81d34ab4dfe2d9002ab729609348e1056fe
Tim Quattrochi <timq82@gmail.com>
2022-11-24 03:37:28 -0500

    added authentication with JWT

--- packages/client/src/utils/api.js
+++ packages/client/src/utils/api.js
@@ -4,6 +4,36 @@ const api = axios.create({
   baseURL: 'http://localhost:3000/api',
 });
 
+const getUserToken = () => {
+  const savedUser = JSON.parse(localStorage.getItem('kickoff_user'));
+  return savedUser ? savedUser.token : '';
+};
+
+api.defaults.headers.post['Content-Type'] = 'application/json';
+api.defaults.headers.common['Authorization'] = getUserToken();
+
+api.interceptors.request.use(
+  (req) => {
+    const token = getUserToken();
+
+    if (token) {
+      req.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return req;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export const setAuthToken = (token) => {
+  if (token) {
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  } else {
+    delete api.defaults.headers.common['Authorization'];
+  }
+};
+
 api.interceptors.response.use((response) => response.data);
 
 export default api;

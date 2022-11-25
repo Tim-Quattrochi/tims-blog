039cfe5d0e26ef4e5da92a082623f89dd8c54ac7
Tim Quattrochi <timq82@gmail.com>
2022-11-25 04:10:14 -0500

    appjs set up express, use middle ware

---
+++ packages/server/app.js
@@ -0,0 +1,37 @@
+import 'dotenv/config';
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import keys from './configs/keys';
+import apiRouter from './routes';
+import cookieParser from 'cookie-parser';
+
+const DB_URL = keys.db.url;
+const API_URL = keys.api.url;
+const PORT = keys.api.port;
+
+mongoose
+  .connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('[Database] Connection Established.'))
+  .catch((err) =>
+    console.log(
+      '[Database] An error occurred connecting to the db: ',
+      err
+    )
+  );
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(API_URL, apiRouter);
+
+app.listen(PORT, () =>
+  console.log(`[Server] Listening on port ${PORT}`)
+);

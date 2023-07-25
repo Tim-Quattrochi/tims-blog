33638538accdc6b918d06c9b53b2ff8819b5b7c6
Tim Quattrochi <timq82@gmail.com>
2023-07-25 00:43:42 -0400

    added some validaiton to the register bootstrap form

--- packages/client/src/pages/SignUpPage.js
+++ packages/client/src/pages/SignUpPage.js
@@ -1,19 +1,23 @@
-import { useState } from 'react';
-import { Button, Container, Form } from 'react-bootstrap';
-import { Link, useNavigate } from 'react-router-dom';
-import api from '../utils/api';
-import { setAuthToken } from '../utils/api';
-import { useProvideAuth } from '../hooks/AuthProvider';
+import { useState } from "react";
+import { Button, Container, Form, Alert } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import api from "../utils/api";
+import { setAuthToken } from "../utils/api";
+import { useProvideAuth } from "../hooks/AuthProvider";
 
 const initialState = {
-  name: '',
-  email: '',
-  password: '',
-  confirmPassword: '',
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+  error: null,
+  touched: false,
 };
 
 const SignUpPage = () => {
   const [data, setData] = useState(initialState);
+  const [validated, setValidated] = useState(false);
+  const [error, setError] = useState(null);
   const auth = useProvideAuth();
 
   const navigate = useNavigate();
@@ -22,13 +26,27 @@ const SignUpPage = () => {
     setData({
       ...data,
       [e.target.name]: e.target.value,
+      touched: true,
     });
   };
 
   const handleSignup = async (e) => {
     e.preventDefault();
+    const form = e.currentTarget;
 
     const { email, password, confirmPassword, name } = data;
+
+    if (password !== confirmPassword) {
+      return setData({
+        ...data,
+        error: "Passwords must match.",
+      });
+    }
+
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
     try {
       console.log(data);
       const res = await auth.signUp(
@@ -41,59 +59,122 @@ const SignUpPage = () => {
       // store the authenticated user in state
       setAuthToken(res.token);
 
-      localStorage.setItem('blogUser', JSON.stringify(res.user));
-      navigate('/blogs');
+      localStorage.setItem("blogUser", JSON.stringify(res.user));
+      navigate("/blogs");
     } catch (error) {
       console.log(error);
+      setData({
+        ...data,
+        error: error.message,
+      });
     }
+    setValidated(true);
   };
   return (
     <Container className="d-flex justify-content-center align-items-center flex-column">
       <h1>Register</h1>
       <Form
         className="row"
-        style={{ width: '50%', maxWidth: '400px' }}
+        style={{ width: "350px", maxWidth: "400px" }}
+        noValidate
+        validated={validated}
         onSubmit={handleSignup}
       >
-        <Form.Group className="mb-3">
+        <Form.Group className="mb-3" controlId="name">
           <Form.Label>Name</Form.Label>
           <Form.Control
+            required
+            isValid={!!data.name.length > 3}
             type="text"
             name="name"
+            autoComplete="name"
             value={data.name}
             onChange={handleChange}
           />
+          <Form.Control.Feedback type="invalid">
+            Please enter your name.
+          </Form.Control.Feedback>
         </Form.Group>
-        <Form.Group className="mb-3">
+        <Form.Group className="mb-3" controlId="email">
           <Form.Label>Email</Form.Label>
           <Form.Control
+            required
             type="text"
             name="email"
+            autoComplete="email"
             value={data.email}
             onChange={handleChange}
           />
+          <Form.Control.Feedback type="invalid">
+            Please enter your email address.
+          </Form.Control.Feedback>
         </Form.Group>
-        <Form.Group className="mb-3">
+        <Form.Group className="mb-3" controlId="password">
           <Form.Label>Password</Form.Label>
           <Form.Control
+            required
             type="password"
             name="password"
+            autoComplete="new-password"
+            isInvalid={data.password.length < 7}
             value={data.password}
             onChange={handleChange}
           />
+          {data.password.length < 7 && data.password !== "" ? (
+            <Form.Control.Feedback type="invalid">
+              Password must be at least 7 characters.
+            </Form.Control.Feedback>
+          ) : (
+            <Form.Control.Feedback type="invalid">
+              Please enter a password.
+            </Form.Control.Feedback>
+          )}
         </Form.Group>
-        <Form.Group className="mb-3">
+        <Form.Group className="mb-3" controlId="confirmPassword">
           <Form.Label>Confirm Password</Form.Label>
           <Form.Control
+            required
             type="password"
             name="confirmPassword"
+            autoComplete="new-password"
             value={data.confirmPassword}
             onChange={handleChange}
+            isValid={
+              data.confirmPassword === data.password && data.touched
+            }
+            isInvalid={
+              data.confirmPassword !== data.password &&
+              data.confirmPassword !== "" &&
+              data.touched
+            }
           />
+          {data.confirmPassword !== data.password &&
+          data.confirmPassword !== "" ? (
+            <Form.Control.Feedback type="invalid">
+              Passwords don't match.
+            </Form.Control.Feedback>
+          ) : (
+            <Form.Control.Feedback type="invalid">
+              Please confirm your password.
+            </Form.Control.Feedback>
+          )}
         </Form.Group>
+
         <Button type="submit" variant="primary">
           Submit
         </Button>
+        <Alert
+          hidden={!data.error}
+          variant="danger"
+          style={{
+            margin: "5px auto 0 auto",
+            width: "20rem",
+            padding: "2px",
+            textAlign: "center",
+          }}
+        >
+          {data.error ? data.error : null}
+        </Alert>
         <Form.Text>
           Already have an account? <Link to="/signin">Log in</Link>
         </Form.Text>

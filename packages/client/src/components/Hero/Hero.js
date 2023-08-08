1c8869bcad55afa5d75636123cd81a29f31ecd8e
Tim Quattrochi <timq82@gmail.com>
2023-08-08 01:48:05 -0400

    hero styling

--- packages/client/src/components/Hero/Hero.js
+++ packages/client/src/components/Hero/Hero.js
@@ -1,28 +1,16 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./hero.css";
-import { Container, Row, Col, Button } from "react-bootstrap";
 
 const Hero = ({ title, para, btnName, path }) => {
-  const navigate = useNavigate();
   return (
     <section className="hero-section">
-      <Container>
-        <Row>
-          <Col>
-            <div className="hero-content">
-              <h1 className="hero-heading">{title}</h1>
-              <p className="p-text">{para}</p>
-              <Button
-                className="cta-button"
-                variant="primary"
-                onClick={() => navigate(`/${path}`)}
-              >
-                {btnName}
-              </Button>
-            </div>
-          </Col>
-        </Row>
-      </Container>
+      <div className="hero-content">
+        <h1 className="hero-heading">{title}</h1>
+        <p className="p-text">{para}</p>
+        <Link className="cta-button" to={`/${path}`}>
+          {btnName}
+        </Link>
+      </div>
     </section>
   );
 };

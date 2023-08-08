f8c1f4ee7ebbaacc928e0baf3d811262081c623e
Tim Quattrochi <timq82@gmail.com>
2023-08-08 01:27:51 -0400

    updated BlogCard styling

--- packages/client/src/components/BlogCard/BlogCard.js
+++ packages/client/src/components/BlogCard/BlogCard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link, useParams, useLocation } from "react-router-dom";
-import { Card, CardGroup, Container } from "react-bootstrap";
 import formatDate from "../../utils/formatDate";
 import "./blogCard.css";
 
@@ -11,36 +10,27 @@ export default function BlogCard({ blog }) {
   const { blogId } = useParams();
 
   return (
-    <Container className="card-body-container">
-      <CardGroup>
-        <Card
-          className="my-4"
-          style={{ width: "auto", margin: "20px" }}
-        >
-          <Card.Body>
-            <Card.Title as="h4" style={{ color: "#007bff" }}>
-              {location.pathname === `/blog/${blogId}` ? (
-                `${blog.title}`
-              ) : (
-                <Link to={`/blog/${blog._id}`}>{blog.title}</Link>
-              )}{" "}
-            </Card.Title>
-
-            <Card.Text
-              as="p"
-              className="text-justify"
-              style={{ whiteSpace: "pre-wrap" }}
-            >
-              {blog?.description}
-            </Card.Text>
-            <Card.Text className="text-muted">
-              {" "}
-              Blogged by {blog.author?.name + " "}
-              on {formatDate(dateStr)}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </CardGroup>
-    </Container>
+    <section id="card-body-container">
+      <div className="card-body">
+        <div className="cs-item-content">
+          <span className="blog-card-date">
+            {formatDate(blog.createdAt)}
+          </span>
+          <h3 className="card-title">
+            {location.pathname === `/blog/${blogId}` ? (
+              `${blog.title}`
+            ) : (
+              <Link to={`/blog/${blog._id}`}>{blog.title}</Link>
+            )}{" "}
+          </h3>
+          <p className="blog-card-text">{blog.description}</p>
+        </div>
+        <div className="blog-card-author">
+          {" "}
+          Blogged by {blog.author?.name + " "}
+          on {formatDate(dateStr)}
+        </div>
+      </div>
+    </section>
   );
 }
